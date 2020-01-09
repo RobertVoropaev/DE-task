4bd4da9f6f7aa9b6915ca0c24d5e739fc56d4941
Robert Voropaev <robertvoropaev11@gmail.com>
2020-01-09 10:53:06 +0300

    edit producer

--- producer.py
+++ producer.py
@@ -1,35 +1,39 @@
-#!/usr/bin/env python
-
-import sys, random, scipy.stats, json, time
+import sys
+import random
+import scipy.stats
+import json
+import time
 from pykafka import KafkaClient
+
+color = ["red", "blue", "green", "yellow"]
+shape = ["triangle", "circle", "rectangle"]
+lognorm = scipy.stats.lognorm(1)
+
+
+def generate_dict():
+    d = dict()
+    d["color"] = color[random.randint(0, 3)]
+    d["shape"] = shape[random.randint(0, 2)]
+    d["size"] = lognorm.rvs(1)[0]
+    return d
+
 
 if __name__ == '__main__':
     client = KafkaClient('localhost:9092')
     topic = client.topics['test']
-    p = topic.get_sync_producer(delivery_reports=True)
 
-    d = dict()
-    frequency = 100
+    with topic.get_producer(delivery_reports=True) as producer:
+        frequency = 100
+        try:
+            while True:
+                d = generate_dict()
+                msg_str = json.dumps(d)
 
-    color = ["red", "blue", "green", "yellow"]
-    shape = ["triangle", "circle", "rectangle"]
-    lognorm = scipy.stats.lognorm(1)
-
-    try:
-        while True:
-            d.clear()
-
-            d["color"] = color[random.randint(0, 3)]
-            d["shape"] = shape[random.randint(0, 2)]
-            d["size"] = lognorm.rvs(1)[0]
-
-            msg_str = json.dumps(d)
-
-            p.produce(bytes(msg_str, encoding="utf-8"), partition_key=b'0')
-
-            time.sleep(1./frequency)
-
-    except KeyboardInterrupt:
-        sys.stderr.write('[Aborted by user]\n')
-    finally:
-        p.stop()
+                producer.produce(bytes(msg_str, encoding="utf-8"),
+                                 partition_key=b'0')
+                
+                time.sleep(1./frequency)
+        except KeyboardInterrupt:
+            sys.stderr.write('[Aborted by user]\n')
+        finally:
+            producer.stop()
