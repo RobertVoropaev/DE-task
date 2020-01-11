ff0b77aee65c26f94a6203e1a35b0906700d2182
RobertVoropaev <robertvoropaev11@gmail.com>
2020-01-11 12:07:41 +0300

    checked consumer

--- consumer.py
+++ consumer.py
@@ -1,24 +1,18 @@
-#!/usr/bin/env python
-
-from pyspark.streaming.kafka import KafkaUtils
-from pyspark.streaming import StreamingContext
 from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
 
 import sys, json
 
-
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        sys.stderr.write('Usage: %s <bootstrap-brokers> <topic>\n' % sys.argv[0])
-        sys.exit(1)
+    broker = "localhost:9092"
+    topic = "test"
 
-    broker = sys.argv[1]
-    topic = sys.argv[2]
-
-    sc = SparkContext(appName="StreamingConsumer")
+    sc = SparkContext(appName="StreamingConsumer", master="local")
+    sc.setLogLevel("WARN")
     ssc = StreamingContext(sc, batchDuration=1)
 
-    rdd = KafkaUtils.createStream(ssc, groupId='0', topics={topic:0}, kafkaParams={'bootstrap.servers': broker})
+    rdd = KafkaUtils.createDirectStream(ssc, [topic], kafkaParams={'metadata.broker.list': broker})
     rdd.pprint()
 
     ssc.start()
